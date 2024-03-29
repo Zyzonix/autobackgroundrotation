882c1d098aef0c35cdcd48851f0b64a56677ada9
zyzonix <zyzonix@gmail.com>
2024-03-29 13:33:02 +0100

    added new simplehtml-plugin

---
+++ main/plugins/simplehtml.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#
+# written by ZyzonixDev
+# published by ZyzonixDevelopments
+#
+# Copyright (c) 2024 ZyzonixDevelopments
+#
+# date created  | 29-03-2024 13:13:32
+# 
+# file          | main/plugins/simplehtml.py
+# project       | autobackgroundrotation
+# file version  | 1.0
+#
+import os
+import traceback
+
+# var storage
+owner_of_images = "www-data:users"
+
+
+# change permissions to image (set owner to http)
+def changeImagePermissions(path):
+    os.system("chown  " + owner_of_images + " " + path)
+
+# set selected image as background
+def changeImageHTTPD(self, logging, imageFilePath):
+    notCompleted = False
+
+    # config file: upload/scp/css/scp.css
+    # path: ../../images/autobackgroundrotation_ticket-overview.jpg
+    # edit scp.css file (background ticketsystem after login) if enabled
+    try:    
+        destination_path = self.simplehtml_basepath + self.simplehtml_defaultname
+        os.system("/usr/bin/cp " + imageFilePath + " " + destination_path)
+        changeImagePermissions(destination_path)
+
+    except:
+        notCompleted = True
+        logging.write(self, "ERROR | Changing background ticket-page failed")
+        logging.writeError(self, traceback.format_exc())
+
+    if not notCompleted:
+        logging.write(self, "INFO  | Image rotation completed")
+        return True
+    # give back if changing was successful to cleanup static file
+    else: return False
+
